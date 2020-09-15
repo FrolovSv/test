958448ef111ac1745b5635ba829df57365e3ee8d
SusPecT <Frolovsv.rzn@gmail.com>
2020-09-16 00:52:30 +0300

    add home work #8

---
+++ src/com/test/HW8.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class HW8 {
+//    Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+    public static void main(String[] args) {
+        for(int i = 1; i <=50; i++)
+            System.out.println(i);
+    }
+
+}
