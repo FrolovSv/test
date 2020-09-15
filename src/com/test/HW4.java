eeddab7fea7d57131c2e4e501248336f1f2843de
SusPecT <Frolovsv.rzn@gmail.com>
2020-09-16 00:24:22 +0300

    add home work #4

---
+++ src/com/test/HW4.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class HW4 {
+    public static void main(String[] args) {
+        int digit;
+        try (Scanner in = new Scanner(System.in)) {
+            // вывод на экран сообщения о неоходимости ввода
+            System.out.print("Введите бинарное число: "));
+            digit = Integer.parseInt(in.nextLine(),2));
+            System.out.println("Данное число в десятичном формате = " + (+digit));
+        } catch (Exception ex) {
+            System.out.println("Ошибка ввода/вывода данных с консоли"));
+            System.exit(0));
+        }
+        System.exit(0);
+    }
+}
