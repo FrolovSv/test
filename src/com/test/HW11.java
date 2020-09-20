e115b96eea7896b579010241787d21d000b8e041
SusPecT <Frolovsv.rzn@gmail.com>
2020-09-21 01:42:52 +0300

    add home work #11

---
+++ src/com/test/HW11.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import java.util.Scanner;
+
+//Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран.
+
+public class HW11 {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            // вывод на экран сообщения о неоходимости ввода
+            System.out.print("Введите любую строку: ");
+            String String = in.nextLine();
+            System.out.println("Вы ввели слудющую строку: " + String);
+        } catch (Exception ex) {
+            System.out.println("Ошибка ввода/вывода данных с консоли");
+            System.exit(0);
+        }
+        System.exit(0);
+    }
+}
