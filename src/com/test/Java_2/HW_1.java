d6d92c7b6ae179ab4d484beeb52451a85e74bf3c
SusPecT <Frolovsv.rzn@gmail.com>
2020-11-19 00:38:22 +0300

    bug fix

--- src/com/test/Java_2/HW_1.java
+++ src/com/test/Java_2/HW_1.java
@@ -9,11 +9,12 @@ import java.util.Scanner;
         результата в консоль. Полученный результат округлять до 4-х
         знаков после запятой.
 
-        Пример работы программы для сложения целых чисел:
+        2. Реализовать базовые операции калькулятора (+,-,/,*) для предыдущего задания
 
-        2. Запушить проект в свой репозиторий на GitHub
-        *Добавить возможность выбора другой операции (сложение,
-        вычитание, деление, умножение)*/
+        3. Поиск максимального элемента в массиве. Для начала задать массив слов.
+        Размерность массива произвольна, задается в консоли.
+        После чего в консоли вводятся слова в количестве равном заданной длине массива.
+        В полученном массиве необходимо найти самое длинное слово. Результат вывести на консоль*/
 /**
  * класс для реализации домашнего задания №1 курса java 2.0
  * @author Frolov S.V.
@@ -29,30 +30,53 @@ public class HW_1 {
      * */
     public static void main(String[] args) {
 
+        int number = 0;
+        while (number < 10) {
+            System.out.println("Number is:"  + number);
+            ++number;
+        }
+
         try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Старт калькулятора. Дроные числа вводятся со знаком \",\" между целой и дробной частью");
-            System.out.println("Введите тип операции. Дотсупные операции (+,-,*,/):");
-            String oper = in.nextLine();
+            System.out.println("Для выбора режима работы введите число: 1 - выполнение калькулятора, 2 - поиск максимального слова в массиве.");
+            if(in.nextInt()==1) {
+                System.out.println("Старт \"Калькулятор\". Дробные числа вводятся со знаком \",\" между целой и дробной частью");
+                System.out.println("Введите тип операции. Дотсупные операции (+,-,*,/):");
+                String operation = in.nextLine();
 
-            System.out.println("Введите первое число:");
-            double digit1 = in.nextDouble();
+                System.out.println("Введите первое число:");
+                double digit1 = in.nextDouble();
 
-            System.out.println("Введите второе число:");
-            double digit2 = in.nextDouble();
+                System.out.println("Введите второе число:");
+                double digit2 = in.nextDouble();
 
-            switch (oper) {
-                case "+":
-                    System.out.printf("Сумма чисел равна: %.4f", (digit1 + digit2));
-                    break;
-                case "-":
-                    System.out.printf("Разность чисел равна: %.4f", (digit1 - digit2));
-                    break;
-                case "*":
-                    System.out.printf("Произведение чисел равна: %.4f", (digit1 * digit2));
-                    break;
-                case "/":
-                    System.out.printf("Деление чисел равна: %.4f", (digit1 / digit2));
-                    break;
+                switch (operation) {
+                    case "+":
+                        System.out.printf("Сумма чисел равна: %.4f", (digit1 + digit2));
+                        break;
+                    case "-":
+                        System.out.printf("Разность чисел равна: %.4f", (digit1 - digit2));
+                        break;
+                    case "*":
+                        System.out.printf("Произведение чисел равна: %.4f", (digit1 * digit2));
+                        break;
+                    case "/":
+                        System.out.printf("Деление чисел равна: %.4f", (digit1 / digit2));
+                        break;
+                }
+            } else {
+                System.out.println("Старт \"Поиск максимального слова в массиве\".");
+                System.out.println("Введите количество слов для массива:");
+                int count = in.nextInt();
+                String[] mass = new String[count];
+                int max = 0;
+                for (int i = 0; i < count; i++) {
+                    System.out.println("Введите " + (i + 1) + "-ое слово:");
+                    mass[i] = in.next();
+                    if (mass[i].length() > max)
+                        max = i;
+                }
+                System.out.println("Самое большое слово в массиве = " + mass[max]);
+
             }
 
 
