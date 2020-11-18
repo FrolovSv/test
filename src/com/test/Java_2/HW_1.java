a57b5049d498bb91d229d845985fd9bc97af0a53
SusPecT <Frolovsv.rzn@gmail.com>
2020-11-18 22:43:42 +0300

    ДЗ №1 по курсу java 2.0

---
+++ src/com/test/Java_2/HW_1.java
@@ -0,0 +1,65 @@
+package com.test.Java_2;
+
+import java.util.Scanner;
+
+/*      1. Начинаем писать калькулятор:
+        Реализовать сложение дробных чисел, которые пользователь
+        вводит в консоль. Использовать комментарии и JavaDoc при
+        описании метода. Использовать форматирование при выводе
+        результата в консоль. Полученный результат округлять до 4-х
+        знаков после запятой.
+
+        Пример работы программы для сложения целых чисел:
+
+        2. Запушить проект в свой репозиторий на GitHub
+        *Добавить возможность выбора другой операции (сложение,
+        вычитание, деление, умножение)*/
+/**
+ * класс для реализации домашнего задания №1 курса java 2.0
+ * @author Frolov S.V.
+ * @version 1.0
+*/
+
+public class HW_1 {
+    /**
+     * "Это основной метод выполняющий заданные функции
+     * @param args параметры заданные при старте приложения
+     * @exception Exception  если были введены не корректные значения то генерируется исключение
+     * @see Exception
+     * */
+    public static void main(String[] args) {
+
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Старт калькулятора");
+            System.out.println("Введите тип операции. Дотсупные операции (+,-,*,/):");
+            String oper = in.nextLine();
+
+            System.out.println("Введите первое число:");
+            double digit1 = in.nextInt();
+
+            System.out.println("Введите второе число:");
+            double digit2 = in.nextInt();
+
+            switch (oper) {
+                case "+":
+                    System.out.format("Сумма чисел равна: %.4f", (digit1 + digit2));
+                    break;
+                case "-":
+                    System.out.format("Сумма чисел равна: %.4f", (digit1 - digit2));
+                    break;
+                case "*":
+                    System.out.format("Сумма чисел равна: %.4f", (digit1 * digit2));
+                    break;
+                case "/":
+                    System.out.format("Сумма чисел равна: %.4f", (digit1 / digit2));
+                    break;
+            }
+
+
+        } catch (Exception ex) {
+            System.out.println("Ошибка ввода/вывода данных с консоли");
+            System.exit(0);
+        }
+        System.exit(0);
+    }
+}
